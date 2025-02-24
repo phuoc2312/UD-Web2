5f6fce7de4f5c93a3911ad02190fccd820a2e5fc
phuoc2312 <phuocmai.231204@gmail.com>
2025-02-24 10:30:32 +0700

    init project

---
+++ src/test/java/com/example/maihuuphuoc/MaihuuphuocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maihuuphuoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaihuuphuocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
